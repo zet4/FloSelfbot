458622ca849dbbb54faf2cd5a30571993a4eb367
Floretta <Moonlington@users.noreply.github.com>
2017-01-20 15:25:12 +0100

    fucked up when stressing out

--- commands/afk.go
+++ commands/afk.go
@@ -71,7 +71,6 @@ func (a *Afk) Message(ctx *Context) {
 			ctx.Conf.MultigameToggled = false
 		}
 		em.Description = "AFKMode is now on!"
-		ctx.Conf.MultigameToggled = false
 		ctx.SendEm(em)
 		ctx.Sess.UserUpdateStatus(discordgo.StatusDoNotDisturb)
 	}
