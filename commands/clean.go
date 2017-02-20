6f989ea61addd015507298ecfd29bc53a3ffedfc
Floretta <Moonlington@users.noreply.github.com>
2017-02-20 12:26:34 +0100

    add: clean until ID

--- commands/clean.go
+++ commands/clean.go
@@ -7,9 +7,17 @@ type Clean struct{}
 
 func (c *Clean) message(ctx *Context) {
 	if len(ctx.Args) != 0 {
-		limit, err := strconv.Atoi(ctx.Args[0])
-		logerror(err)
-		msgs, err := ctx.Sess.ChannelMessages(ctx.Mess.ChannelID, limit, ctx.Mess.ID, "", "")
+		var mID string
+		limit, err := strconv.Atoi(ctx.Argstr)
+		if err != nil {
+			ctx.QuickSendEm("Invalid amount specified")
+			return
+		}
+		if limit > 100 {
+			mID = ctx.Argstr
+			limit = 100
+		}
+		msgs, err := ctx.Sess.ChannelMessages(ctx.Mess.ChannelID, limit, ctx.Mess.ID, mID, "")
 		logerror(err)
 		for _, msg := range msgs {
 			if msg.Author.ID == ctx.Sess.State.User.ID {
@@ -25,8 +33,8 @@ func (c *Clean) message(ctx *Context) {
 }
 
 func (c *Clean) description() string { return "Cleans up your messages" }
-func (c *Clean) usage() string       { return "<amount>" }
+func (c *Clean) usage() string       { return "<amount>/<messageID>" }
 func (c *Clean) detailed() string {
-	return "If you realise you have been spamming a little, this is the command to use then."
+	return "If you realise you have been spamming a little, this is the command to use then.\nIf message ID is specified, delete everything you posted until that message. (This does not include that message)"
 }
 func (c *Clean) subcommands() map[string]Command { return make(map[string]Command) }
