7163af54caf623c8fe96f56d2e8c43655afc4116
Floretta <Moonlington@users.noreply.github.com>
2017-03-27 16:05:09 +0200

    fix: Serverinfo crashing in dms

--- commands/serverinfo.go
+++ commands/serverinfo.go
@@ -13,73 +13,77 @@ import (
 type Sinfo struct{}
 
 func (s *Sinfo) message(ctx *Context) {
-	em := createEmbed(ctx)
-	em.Author = &discordgo.MessageEmbedAuthor{
-		Name:    fmt.Sprintf("Server Info: %s", ctx.Guild.Name),
-		IconURL: "https://twemoji.maxcdn.com/36x36/2139.png",
-	}
-	if ctx.Guild.Icon != "" {
-		em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: fmt.Sprintf("https://cdn.discordapp.com/icons/%s/%s.jpg", ctx.Guild.ID, ctx.Guild.Icon)}
-	}
-	em.Fields = make([]*discordgo.MessageEmbedField, 0)
-	if len(ctx.Guild.Emojis) > 0 {
-		var buf bytes.Buffer
-		for _, emote := range ctx.Guild.Emojis[0:int(math.Min(float64(40), float64(len(ctx.Guild.Emojis))))] {
-			fmt.Fprintf(&buf, "<:x:%s>", emote.ID)
+	if !ctx.Channel.IsPrivate {
+		em := createEmbed(ctx)
+		em.Author = &discordgo.MessageEmbedAuthor{
+			Name:    fmt.Sprintf("Server Info: %s", ctx.Guild.Name),
+			IconURL: "https://twemoji.maxcdn.com/36x36/2139.png",
+		}
+		if ctx.Guild.Icon != "" {
+			em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: fmt.Sprintf("https://cdn.discordapp.com/icons/%s/%s.jpg", ctx.Guild.ID, ctx.Guild.Icon)}
+		}
+		em.Fields = make([]*discordgo.MessageEmbedField, 0)
+		if len(ctx.Guild.Emojis) > 0 {
+			var buf bytes.Buffer
+			for _, emote := range ctx.Guild.Emojis[0:int(math.Min(float64(40), float64(len(ctx.Guild.Emojis))))] {
+				fmt.Fprintf(&buf, "<:x:%s>", emote.ID)
+			}
+			em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
+				Name:  "Emojis (up to 40)",
+				Value: buf.String(),
+			})
 		}
 		em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
-			Name:  "Emojis (up to 40)",
-			Value: buf.String(),
+			Name:   "Owner",
+			Value:  "<@" + ctx.Guild.OwnerID + ">",
+			Inline: true,
 		})
-	}
-	em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
-		Name:   "Owner",
-		Value:  "<@" + ctx.Guild.OwnerID + ">",
-		Inline: true,
-	})
-	em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
-		Name:   "Roles",
-		Value:  strconv.Itoa(len(ctx.Guild.Roles)),
-		Inline: true,
-	})
-	em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
-		Name:   "Emojis",
-		Value:  strconv.Itoa(len(ctx.Guild.Emojis)),
-		Inline: true,
-	})
-	var regular, voice, hidden int
-	for _, ch := range ctx.Guild.Channels {
-		regular++
-		if ch.Type == "voice" {
-			voice++
-			continue
+		em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
+			Name:   "Roles",
+			Value:  strconv.Itoa(len(ctx.Guild.Roles)),
+			Inline: true,
+		})
+		em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
+			Name:   "Emojis",
+			Value:  strconv.Itoa(len(ctx.Guild.Emojis)),
+			Inline: true,
+		})
+		var regular, voice, hidden int
+		for _, ch := range ctx.Guild.Channels {
+			regular++
+			if ch.Type == "voice" {
+				voice++
+				continue
+			}
+			perms, err := ctx.Sess.State.UserChannelPermissions(ctx.Sess.State.User.ID, ch.ID)
+			if err == nil && perms&discordgo.PermissionReadMessages == 0 {
+				hidden++
+			}
 		}
-		perms, err := ctx.Sess.State.UserChannelPermissions(ctx.Sess.State.User.ID, ch.ID)
-		if err == nil && perms&discordgo.PermissionReadMessages == 0 {
-			hidden++
+		em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
+			Name:   "Channels",
+			Value:  fmt.Sprintf("%d channels (%d voice, %d hidden)", regular, voice, hidden),
+			Inline: true,
+		})
+		var bots int
+		for _, mb := range ctx.Guild.Members {
+			if mb.User.Bot {
+				bots++
+			}
 		}
-	}
-	em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
-		Name:   "Channels",
-		Value:  fmt.Sprintf("%d channels (%d voice, %d hidden)", regular, voice, hidden),
-		Inline: true,
-	})
-	var bots int
-	for _, mb := range ctx.Guild.Members {
-		if mb.User.Bot {
-			bots++
+		var botmsg string
+		if bots > 0 {
+			botmsg = fmt.Sprintf(", %d or %.2f%% of members are bots", bots, (float64(bots)*float64(100))/float64(len(ctx.Guild.Members)))
 		}
+		em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
+			Name:   "Members",
+			Value:  fmt.Sprintf("%d total%s", len(ctx.Guild.Members), botmsg),
+			Inline: true,
+		})
+		ctx.SendEm(em)
+	} else {
+		ctx.QuickSendEm("Command can't be used in DMs!")
 	}
-	var botmsg string
-	if bots > 0 {
-		botmsg = fmt.Sprintf(", %d or %.2f%% of members are bots", bots, (float64(bots)*float64(100))/float64(len(ctx.Guild.Members)))
-	}
-	em.Fields = append(em.Fields, &discordgo.MessageEmbedField{
-		Name:   "Members",
-		Value:  fmt.Sprintf("%d total%s", len(ctx.Guild.Members), botmsg),
-		Inline: true,
-	})
-	ctx.SendEm(em)
 }
 
 func (s *Sinfo) description() string             { return "Server info" }
